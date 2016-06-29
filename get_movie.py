11c02f0c7d49f40aa2434726d528600057a0554a
sogwsc <95622135@qq.com>
2016-06-29 17:22:16 +0800

    V1.0

---
+++ get_movie.py
@@ -0,0 +1,48 @@
+# -*-coding:utf-8 -*-
+
+
+import MySQLdb
+from globe_var import *
+import sys
+
+
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
+def get_movie_info(movie_type):
+    if movie_type == '全部' or movie_type in movie_types:
+        content = query_mysql(movie_type)
+        return content
+    else:
+        content = '请输入正确的类型'
+        return content
+
+
+def query_mysql(movie_type):
+        conn = MySQLdb.connect(host=mysql_info['host'], user=mysql_info['user'], passwd=mysql_info['passwd'],
+                               db=mysql_info['db'], port=int(mysql_info['port']), charset='utf8')
+        cur = conn.cursor()
+        if movie_type == '全部':
+            sql = 'SELECT movie_name,movie_score,people_num,movie_url FROM  movie_info WHERE movie_id >= ((SELECT ' \
+                  'MAX(movie_id) FROM movie_info)-(SELECT MIN(movie_id) FROM movie_info)) * RAND() + (SELECT MIN(movie_id) ' \
+                  'FROM movie_info)  LIMIT 3'
+            print(sql)
+        else:
+            sql = 'SELECT movie_name,movie_score,people_num,movie_url FROM  movie_info WHERE movie_id >= ((SELECT ' \
+                  'MAX(movie_id) FROM movie_info)-(SELECT MIN(movie_id) FROM movie_info)) * RAND() + (SELECT MIN(movie_id) ' \
+                  'FROM movie_info) AND movie_type LIKE "%%%s%%"  LIMIT 3' % movie_type
+            print(sql)
+        cur.execute(sql)
+        result = cur.fetchmany(3)
+        conn.cursor().close()
+        conn.close()
+        movie1 = '电影名:'+result[0][0].encode('utf-8')+' 电影评分:'+result[0][1]+' 评分人数:'+result[0][2]+' 豆瓣电影链接:'\
+                 +result[0][3].encode('utf-8')
+        movie2 = '电影名:'+result[1][0].encode('utf-8')+' 电影评分:'+result[1][1]+' 评分人数:'+result[1][2]+' 豆瓣电影链接:'\
+                 +result[1][3].encode('utf-8')
+        movie3 = '电影名:'+result[2][0].encode('utf-8')+' 电影评分:'+result[2][1]+' 评分人数:'+result[2][2]+' 豆瓣电影链接:'\
+                 +result[2][3].encode('utf-8')
+        content = '1.'+movie1+' 2.'+movie2+' 3.'+movie3
+        return content
+
+
---
+++ wechat.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+from flask import Flask, request, make_response
+import time
+import hashlib
+from xml.etree.ElementTree import *
+from get_movie import *
+
+
+error_msg = '没有结果，请稍后再试'
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET', 'POST'])
+def wechat_check():
+    if request.method == 'GET':
+        token = 'wan1987'
+        query = request.args
+        signature = query.get('signature', '')
+        timestamp = query.get('timestamp', '')
+        nonce = query.get('nonce', '')
+        echostr = query.get('echostr', '')
+        s = [timestamp, nonce, token]
+        s.sort()
+        s = ''.join(s)
+        if hashlib.sha1(s).hexdigest() == signature:
+            return make_response(echostr)
+
+    recv_xml = fromstring(request.data)
+    ToUserName = recv_xml.find("ToUserName").text
+    FromUserName = recv_xml.find("FromUserName").text
+    recv_Content = recv_xml.find("Content").text
+    reply = "<xml><ToUserName><![CDATA[%s]]></ToUserName><FromUserName><![CDATA[%s]]></FromUserName><CreateTime>%s</Crea" \
+            "teTime><MsgType><![CDATA[text]]></MsgType><Content><![CDATA[%s]]></Content><FuncFlag>0</FuncFlag></xml>"
+
+    reply_context = query_mysql(recv_Content)
+    if reply_context == None:
+        reply_context = error_msg
+    response = make_response(reply % (FromUserName, ToUserName, str(int(time.time())), reply_context))
+    response.content_type = 'application/xml'
+    return response
+
+
+if __name__ == '__main__':
+    app.run(host='192.168.198.190', port=80)
