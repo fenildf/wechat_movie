a21fc74dd9be05d0ed3bbd1b174336a5776856ca
sogwsc <95622135@qq.com>
2016-07-04 15:47:52 +0800

    增加limit限制，提高查找速度

--- serch_movie.py
+++ serch_movie.py
@@ -8,7 +8,7 @@ def serch_movie(movie_name):
     key = {'wp': '0', 'ty': 'gn', 'op': 'gn', 'q': movie_name, 'q': movie_name}
     r = requests.get('http://www.wangpansou.cn/s.php', params=key)
     soup = BeautifulSoup(r.text, "html.parser")
-    urls = soup.find_all("a", attrs={"class": "cse-search-result_content_item_top_a"})
+    urls = soup.find_all("a", attrs={"class": "cse-search-result_content_item_top_a"}, limit=3)
     if len(urls) > 3:
         movie_url = "网盘地址："+'1.'+urls[0].get('href')+'\n2.'+urls[1].get('href')+'\n3.'+urls[2].get('href')
     else:
