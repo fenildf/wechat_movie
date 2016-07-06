7d322f2242ceec4df138951e8b3e116488d02f2f
Mr.shu <95622135@qq.com>
2016-07-06 09:57:30 +0800

    Update globe_var.py

--- globe_var.py
+++ globe_var.py
@@ -4,4 +4,4 @@ movie_types = {'全部', '爱情', '喜剧', '动画', '剧情', '科幻', '动�
                '战争', '短片', '魔幻', '黑色幽默', '传记', '情色', '暴力', '感人', '动画短片', '音乐', '家庭', '童年', '黑帮', '浪漫', '女性',
                '同志', '史诗', '童话', 'cult'}
 
-mysql_info = dict(host='123.207.96.207', user='root', passwd='root123', db='wx_test', port='3306')
+mysql_info = dict(host='*.*.*.*', user='root', passwd='root123', db='wx_test', port='3306')
